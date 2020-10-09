0ae45590a3792d05160ba0359e9c98691a43ec6d
vlaaad <v.proczenko@gmail.com>
2020-10-09 20:15:57 +0200

    Add submit-with-form example

---
+++ examples/e06_submit_with_form.clj
@@ -0,0 +1,27 @@
+(ns e06-submit-with-form
+  (:require [vlaaad.reveal :as r]
+            [vlaaad.reveal.ext :as rx]))
+
+;; This example shows how to create a custom Reveal UI that allows submitting
+;; evaluation results with code forms that produced these results (similar to
+;; REBL's submit function)
+
+(def ui (r/ui))
+
+(defn submit-with-form [form value]
+  (ui (rx/stream-as-is
+        (rx/vertical
+          (rx/as form
+            (rx/raw-string (pr-str form) {:fill :util}))
+          (rx/horizontal
+            (rx/raw-string "=>" {:fill :util})
+            rx/separator
+            (rx/stream value))))))
+
+(comment
+  ;; example usage
+  (submit-with-form '(+ 1 2 3) 6))
+
+;; reveal shows:
+;; (+ 1 2 3)
+;; => 6
