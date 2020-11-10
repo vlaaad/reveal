fde48331382d55e758a5a88ec1d80adeed806cb3
vlaaad <v.proczenko@gmail.com>
2020-11-10 20:18:45 +0100

    Don't create commits for releases

--- release.ps1
+++ release.ps1
@@ -7,16 +7,18 @@ function invoke {
 if (!((invoke git rev-parse --abbrev-ref HEAD) -eq "master")) {
     throw "not on master"
 }
+try { invoke git diff-index --quiet HEAD -- } catch { Throw "index is not clean" }
+
 $version = "1.1.$(invoke git rev-list HEAD --count)"
 
 clj -A:build -M -m version $version $(invoke git rev-parse HEAD)
 clj -Spom
-invoke git commit -am "Release $version"
 invoke git tag $version
 invoke git push
 invoke git push origin $version
 clj -A:depstar "$version.jar"
 clj -A:build -M -m deploy "$version.jar" (Read-Host -Prompt "Username") (Read-Host -Prompt "Token" -AsSecureString | ConvertFrom-SecureString -AsPlainText)
 rm "$version.jar"
+invoke git checkout -- pom.xml
 
 
