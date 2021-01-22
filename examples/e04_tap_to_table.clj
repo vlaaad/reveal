5280dc5ee6f89840fe5f0a9193c20b69e59eab9c
vlaaad <v.proczenko@gmail.com>
2021-01-22 22:22:48 +0100

    Update tap-to-table example

--- examples/e04_tap_to_table.clj
+++ examples/e04_tap_to_table.clj
@@ -1,25 +1,26 @@
-(ns e04-tap-to-table
-  (:require [vlaaad.reveal.ext :as rx]))
+(ns e04-tap-to-table)
 
-;; Evaluating the following form will return a value with `view` action that
+;; Evaluating the following form will make reveal open a view that
 ;; will always show last tapped value in a table. This view guesses columns
 ;; by only looking at the first item in a coll, so it might not show the whole
-;; tapped value.
+;; tapped value. When the view is closed, it will remove the tap listener
 
-(let [last-tap (atom nil)]
-  (add-tap #(reset! last-tap %))
-  {:fx/type rx/observable-view
-   :ref last-tap
-   :fn (fn [x]
-         (if (or (nil? x) (string? x) (not (seqable? x)))
-           {:fx/type rx/table-view
-            :items [x]
-            :columns [{:fn identity :header 'value}]}
-           (let [head (first x)]
-             {:fx/type rx/table-view
-              :items x
-              :columns (cond
-                         (map? head) (for [k (keys head)] {:header k :fn #(get % k)})
-                         (map-entry? head) [{:header 'key :fn key} {:header 'val :fn val}]
-                         (indexed? head) (for [i (range (count head))] {:header i :fn #(nth % i)})
-                         :else [{:header 'item :fn identity}])})))})
+{:vlaaad.reveal/command
+ '(open-view
+    {:fx/type observable-view
+     :subscribe (fn [notify]
+                  (add-tap notify)
+                  #(remove-tap notify))
+     :fn (fn [x]
+           (if (or (nil? x) (string? x) (not (seqable? x)))
+             {:fx/type table-view
+              :items [x]
+              :columns [{:fn identity :header 'value}]}
+             (let [head (first x)]
+               {:fx/type table-view
+                :items x
+                :columns (cond
+                           (map? head) (for [k (keys head)] {:header k :fn #(get % k)})
+                           (map-entry? head) [{:header 'key :fn key} {:header 'val :fn val}]
+                           (indexed? head) (for [i (range (count head))] {:header i :fn #(nth % i)})
+                           :else [{:header 'item :fn identity}])})))})}
