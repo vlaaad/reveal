8f4f0296a545fb6e8437160bc68eb90fec6e2a08
vlaaad <v.proczenko@gmail.com>
2021-12-08 10:45:47 +0100

    Improve docs

--- src/vlaaad/reveal/doc.clj
+++ src/vlaaad/reveal/doc.clj
@@ -4,164 +4,11 @@
             [clojure.string :as str]
             [clojure.spec.alpha :as s]
             [clojure.java.io :as io])
-  (:import [com.vladsch.flexmark.parser Parser]
-           [com.vladsch.flexmark.util.data MutableDataSet]
-           [com.vladsch.flexmark.util.ast Node Document]
-           [com.vladsch.flexmark.ast Paragraph SoftLineBreak HardLineBreak ThematicBreak
-                                     Text BulletList ListItem Heading BlockQuote
-                                     FencedCodeBlock IndentedCodeBlock
-                                     OrderedList DelimitedNodeImpl HtmlInline TextBase
-                                     AutoLink InlineLinkNode MailLink]
-           [com.vladsch.flexmark.ext.wikilink WikiLink WikiLinkExtension]
-           [com.vladsch.flexmark.ext.autolink AutolinkExtension]
-           [clojure.lang Namespace RT]
+  (:import [clojure.lang Namespace RT]
            [java.io LineNumberReader InputStreamReader PushbackReader]
            [java.net URL]))
 
-(def ^:dynamic ^:private *doc-ns*)
-
-(def ^Parser parser
-  (.build (Parser/builder
-            (doto (MutableDataSet.)
-              (.set Parser/EXTENSIONS [(WikiLinkExtension/create)
-                                       (AutolinkExtension/create)])))))
-
 (set! *warn-on-reflection* true)
-
-(defprotocol md->sf
-  (->sf [this]))
-
-(defn- children [^Node node]
-  (->> node
-       .getFirstChild
-       (iterate #(.getNext ^Node %))
-       (take-while some?)))
-
-(defn- line-break? [x]
-  (or (instance? SoftLineBreak x)
-      (instance? HardLineBreak x)))
-
-(defn- partition-lines [xs]
-  (->> xs
-       (partition-by line-break?)
-       (remove #(-> % first line-break?))))
-
-(extend-protocol md->sf
-  Node
-  (->sf [this]
-    (stream/raw-string (.getChars this) {:fill :string}))
-  Document
-  (->sf [this]
-    (apply stream/vertical (map ->sf (children this))))
-  Paragraph
-  (->sf [this]
-    (cond-> (->> this
-                 children
-                 partition-lines
-                 (map (fn [children]
-                        (apply stream/horizontal (map ->sf children))))
-                 (apply stream/vertical))
-            (.isTrailingBlankLine this)
-            (stream/horizontal (stream/raw-string "\n"))))
-  Text
-  (->sf [this]
-    (stream/raw-string (.getChars this) {:fill :string}))
-  TextBase
-  (->sf [this]
-    (apply stream/horizontal (map ->sf (children this))))
-  AutoLink
-  (->sf [this]
-    (let [link (str (.getChars this))]
-      (stream/as link
-        (stream/raw-string link {:fill :scalar}))))
-  MailLink
-  (->sf [this]
-    (stream/raw-string (.getChars this) {:fill :string}))
-  InlineLinkNode
-  (->sf [this]
-    (stream/horizontal
-      (stream/raw-string (.getTextOpeningMarker this) {:fill :util})
-      (stream/raw-string (.getText this) {:fill :string})
-      (stream/raw-string (str (.getTextClosingMarker this)
-                              (.getLinkOpeningMarker this))
-                         {:fill :util})
-      (stream/as (str (.getUrl this))
-        (stream/raw-string (.getUrl this) {:fill :scalar}))
-      (stream/raw-string (str (.getLinkClosingMarker this)) {:fill :util})))
-  DelimitedNodeImpl
-  (->sf [this]
-    (let [marker (str (.getOpeningMarker this))
-          marker-color (case marker
-                         "*" :string
-                         :util)]
-      (stream/horizontal
-        (stream/raw-string marker {:fill marker-color})
-        (stream/raw-string (.getText this) {:fill (case marker
-                                                    "`" :symbol
-                                                    :string)})
-        (stream/raw-string (.getClosingMarker this) {:fill marker-color}))))
-  ThematicBreak
-  (->sf [this]
-    (stream/raw-string (.getChars this) {:fill :string}))
-  BulletList
-  (->sf [this]
-    (let [marker (str (.getOpeningMarker this) " ")]
-      (->> this
-           children
-           (map (fn [child]
-                  (stream/horizontal
-                    (stream/raw-string marker {:fill :string})
-                    (->sf child))))
-           (apply stream/vertical))))
-  OrderedList
-  (->sf [this]
-    (->> this
-         children
-         (map (fn [i child]
-                (stream/horizontal
-                  (stream/raw-string (str i (.getDelimiter this) " ") {:fill :string})
-                  (->sf child)))
-              (iterate inc (.getStartNumber this)))
-         (apply stream/vertical)))
-  ListItem
-  (->sf [this]
-    (->> this children (map ->sf) (apply stream/vertical)))
-  Heading
-  (->sf [this]
-    (stream/horizontal
-      (stream/raw-string (str (.getOpeningMarker this) " ") {:fill :util})
-      (->> this children (map ->sf) (apply stream/horizontal))
-      (stream/raw-string "\n")))
-  BlockQuote
-  (->sf [this]
-    (stream/horizontal
-      (stream/raw-string "> " {:fill :util})
-      (apply stream/horizontal (map ->sf (children this)))))
-  FencedCodeBlock
-  (->sf [this]
-    (stream/vertical
-      (stream/raw-string (str (.getOpeningFence this) (.getInfo this)) {:fill :util})
-      (stream/raw-string (str/trim-newline (.getChildChars this)) {:fill :symbol})
-      (stream/raw-string (.getClosingFence this) {:fill :util})))
-  IndentedCodeBlock
-  (->sf [this]
-    (stream/raw-string (str (.getChars this)) {:fill :symbol}))
-  HtmlInline
-  (->sf [this]
-    (stream/raw-string (str (.getChars this)) {:fill :util}))
-  WikiLink
-  (->sf [this]
-    (stream/horizontal
-      (stream/raw-string (.getOpeningMarker this) {:fill :util})
-      (let [sym (symbol (str (.getChildChars this)))]
-        (if-let [target (or (ns-resolve *doc-ns* sym)
-                            (find-ns sym))]
-          (stream/as target
-            (stream/raw-string
-              (.getChildChars this)
-              {:fill :object}))
-          (stream/raw-string (.getChildChars this) {:fill :string})))
-      (stream/raw-string (.getClosingMarker this) {:fill :util}))))
 
 (defn parse [docstring ns]
   (let [lines (str/split-lines docstring)
@@ -171,10 +18,26 @@
                             (map #(count (take-while #{\space} %))))
         indent (if (seq indented-lines) (apply min indented-lines) 0)
         re-indent (re-pattern (str "^\\s{" indent "}"))]
-    (binding [*doc-ns* (the-ns ns)]
-      (->sf (.parse parser ^String (->> lines
-                                        (map #(str/replace % re-indent ""))
-                                        (str/join "\n")))))))
+    (let [ns (the-ns ns)]
+      (->> lines
+           (map #(str/replace % re-indent ""))
+           (map (fn [line]
+                  (->> line
+                       (re-seq #"\[\[[^\]]+\]\]|\[[^\[]*|[^\[]*")
+                       (remove str/blank?)
+                       (map #(if (str/starts-with? % "[[")
+                               (stream/horizontal
+                                 (stream/raw-string "[[" {:fill :util})
+                                 (let [sym-str (subs % 2 (- (count %) 2))
+                                       sym (symbol sym-str)]
+                                   (if-let [target (or (ns-resolve ns sym) (find-ns sym))]
+                                     (stream/as target
+                                       (stream/raw-string sym-str {:fill :object}))
+                                     (stream/raw-string sym-str {:fill :string})))
+                                 (stream/raw-string "]]" {:fill :util}))
+                               (stream/raw-string % {:fill :string})))
+                       (apply stream/horizontal))))
+           stream/vertically))))
 
 (defn- for-var [var]
   (let [m (meta var)]
@@ -183,19 +46,22 @@
       (concat
         [(apply stream/horizontal
                 (stream/stream var)
-                (when (:macro m)
-                  [stream/separator
-                   (stream/raw-string "(macro)" {:fill :util})]))]
+                (let [extras (cond-> []
+                               (:macro m) (conj "macro")
+                               (:added m) (conj (str "since " (:added m))))]
+                  (when-not (empty? extras)
+                    [stream/separator
+                     (stream/raw-string (str "(" (str/join ", " extras) ")") {:fill :util})])))]
         (when-let [arglists (:arglists m)]
           (map stream/stream arglists))
         (when-let [deprecated (:deprecated m)]
           [stream/separator
            (if (string? deprecated)
              (stream/horizontal
-               (stream/raw-string "Deprecated." {:fill :string})
+               (stream/raw-string "Deprecated." {:fill :error})
                stream/separator
                (parse deprecated (:ns m)))
-             (stream/raw-string "Deprecated." {:fill :string}))])
+             (stream/raw-string "Deprecated." {:fill :error}))])
         (when-let [doc (:doc m)]
           [stream/separator
            (parse doc (:ns m))
@@ -211,10 +77,14 @@
            (stream/stream (s/describe spec))])))))
 
 (defn- for-ns [ns]
-  (stream/vertical
-    (stream/stream ns)
-    stream/separator
-    (parse (:doc (meta ns)) ns)))
+  (let [m (meta ns)]
+    (apply
+      stream/vertical
+      (concat
+        [(stream/stream ns)]
+        (when-let [author (:author m)]
+          [(stream/raw-string (str "by " author) {:fill :util})])
+        [stream/separator (parse (:doc m) ns)]))))
 
 (defn- for-spec [k]
   (stream/vertical
@@ -245,7 +115,7 @@
 
     (simple-symbol? x)
     (when-let [ns (find-ns x)]
-      (when (:doc (meta x))
+      (when (:doc (meta ns))
         #(for-ns ns)))
 
     (qualified-symbol? x)
